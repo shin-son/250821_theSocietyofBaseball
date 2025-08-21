dedc92b3695c069dbdeb3a1040ade035468d5c3e
shin.son <thstls110534@naver.com>
2025-08-21 13:25:08 +0900

    added answer text

--- 0821_project/attendance_test.cpp
+++ 0821_project/attendance_test.cpp
@@ -5,17 +5,13 @@
 
 class AttendanceFixture : public testing::Test {
 public:
-    std::string& RunProgramOnceAndGetOutput() {
+    std::string& getAttendanceAnswer() {
         static bool isDone = false;
         static std::string captured;
 
         if (isDone == false) {
-            std::ifstream fin("attendance_weekday_500.txt");
-
-            testing::internal::CaptureStdout();
-            load();
-            compute();
-            captured = testing::internal::GetCapturedStdout();
+            std::ifstream fin("attendance_answer.txt");
+            captured.assign((std::istreambuf_iterator<char>(fin)), std::istreambuf_iterator<char>());
             isDone = true;
         }
         return captured;
@@ -30,7 +26,7 @@ public:
 };
 
 TEST_F(AttendanceFixture, PrintsHeaderAndRemovedSectionMarker) {
-    const std::string& answer = RunProgramOnceAndGetOutput();
+    const std::string& answer = getAttendanceAnswer();
     EXPECT_NE(answer.find("NAME : "), std::string::npos);
     EXPECT_NE(answer.find("POINT : "), std::string::npos);
     EXPECT_NE(answer.find("GRADE : "), std::string::npos);
@@ -39,36 +35,36 @@ TEST_F(AttendanceFixture, PrintsHeaderAndRemovedSectionMarker) {
 }
 
 TEST_F(AttendanceFixture, ContainsUmarSummaryLine) {
-    const std::string& answer = RunProgramOnceAndGetOutput();
+    const std::string& answer = getAttendanceAnswer();
     EXPECT_NE(answer.find("NAME : Umar, POINT : 48, GRADE : SILVER"), std::string::npos);
 }
 
 TEST_F(AttendanceFixture, ContainsZaneSummaryLine) {
-    const std::string& answer = RunProgramOnceAndGetOutput();
+    const std::string& answer = getAttendanceAnswer();
     EXPECT_NE(answer.find("NAME : Zane, POINT : 1, GRADE : NORMAL"), std::string::npos);
 }
 
 TEST_F(AttendanceFixture, HasAtLeastOneNormalMember) {
-    const std::string& answer = RunProgramOnceAndGetOutput();
+    const std::string& answer = getAttendanceAnswer();
     EXPECT_NE(answer.find("GRADE : NORMAL"), std::string::npos);
 }
 
 TEST_F(AttendanceFixture, ZaneAppearsInRemovedSection) {
-    const std::string& answer = RunProgramOnceAndGetOutput();
+    const std::string& answer = getAttendanceAnswer();
     const std::string removedPlayer = GetRemovedSection(answer);
     ASSERT_FALSE(removedPlayer.empty()) << "Removed section not found";
     EXPECT_NE(removedPlayer.find("Zane"), std::string::npos);
 }
 
 TEST_F(AttendanceFixture, BobAppearsInRemovedSection) {
-    const std::string& answer = RunProgramOnceAndGetOutput();
+    const std::string& answer = getAttendanceAnswer();
     const std::string removedPlayer = GetRemovedSection(answer);
     ASSERT_FALSE(removedPlayer.empty()) << "Removed section not found";
     EXPECT_NE(removedPlayer.find("Bob"), std::string::npos);
 }
 
 TEST_F(AttendanceFixture, XenaDoesNotAppearInRemovedSection) {
-    const std::string& answer = RunProgramOnceAndGetOutput();
+    const std::string& answer = getAttendanceAnswer();
     const std::string removedPlayer = GetRemovedSection(answer);
     ASSERT_FALSE(removedPlayer.empty()) << "Removed section not found";
     EXPECT_EQ(removedPlayer.find("Xena"), std::string::npos);
