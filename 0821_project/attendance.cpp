69944925a702765a2e0b3f1d6fcd207ba1c6e11d
shin.son <thstls110534@naver.com>
2025-08-21 10:47:31 +0900

    Added the D1 refactoring

--- 0821_project/attendance.cpp
+++ 0821_project/attendance.cpp
@@ -7,134 +7,99 @@
 
 using namespace std;
 
-struct Node {
-	string w;
-	string wk;
+map<string, int> rollBook;
+int totalUniformNumber = 0;
+
+int bonusPoint[100][100];
+int totalPoint[100];
+int memberGrade[100];
+string memberNameArray[100];
+string gradeList[3] = { "NORMAL", "SILVER", "GOLD" };
+
+int trainingDayAttendanceCount[100];
+int weekendAttendanceCount[100];
+
+enum {
+	wednesday = 0,
+	weekend = 1,
 };
 
-map<string, int> id1;
-int id_cnt = 0;
-
-//dat[사용자ID][요일]
-int dat[100][100];
-int points[100];
-int grade[100];
-string names[100];
-
-int wed[100];
-int weeken[100];
-
-void input2(string w, string wk) {
-	//ID 부여
-	if (id1.count(w) == 0) {
-		id1.insert({ w, ++id_cnt });
-
-		if (w == "Daisy") {
-			int debug = 1;
-		}
-
-		names[id_cnt] = w;
+void updateAttendance(string name) {
+	if (rollBook.count(name) == 0) {
+		rollBook.insert({ name, ++totalUniformNumber });
+		memberNameArray[totalUniformNumber] = name;
 	}
-	int id2 = id1[w];
-
-	//디버깅용
-	if (w == "Daisy") {
-		int debug = 1;
-	}
-
-
-	int add_point = 0;
-	int index = 0;
-	if (wk == "monday") {
-		index = 0;
-		add_point++;
-	}
-	if (wk == "tuesday") {
-		index = 1;
-		add_point++;
-	}
-	if (wk == "wednesday") {
-		index = 2;
-		add_point += 3;
-		wed[id2] += 1;
-	}
-	if (wk == "thursday") {
-		index = 3;
-		add_point++;
-	}
-	if (wk == "friday") {
-		index = 4;
-		add_point++;
-	}
-	if (wk == "saturday") {
-		index = 5;
-		add_point += 2;
-		weeken[id2] += 1;
-	}
-	if (wk == "sunday") {
-		index = 6;
-		add_point += 2;
-		weeken[id2] += 1;
-	}
-
-	//사용자ID별 요일 데이터에 1씩 증가
-	dat[id2][index] += 1;
-	points[id2] += add_point;
 }
 
-void input() {
-	ifstream fin{ "attendance_weekday_500.txt" }; //500개 데이터 입력
-	for (int i = 0; i < 500; i++) {
-		string t1, t2;
-		fin >> t1 >> t2;
-		input2(t1, t2);
+void updateScore(string name, string day) {
+	int uniformNumber = rollBook[name];
+
+	if (day == "wednesday") {
+		totalPoint[uniformNumber] += 3;
+		bonusPoint[uniformNumber][wednesday] += 1;
+		trainingDayAttendanceCount[uniformNumber]++;
 	}
+	else if (day == "saturday" || day == "sunday") {
+		totalPoint[uniformNumber] += 2;
+		bonusPoint[uniformNumber][weekend] += 1;
+		weekendAttendanceCount[uniformNumber]++;
+	}
+	else {
+		totalPoint[uniformNumber]++;
+	}
+}
 
-	for (int i = 1; i <= id_cnt; i++) {
-		if (dat[i][2] > 9) {
-			points[i] += 10;
+void updateAttendanceAndScore(string name, string day) {
+	updateAttendance(name);
+	updateScore(name, day);
+}
+
+void load() {
+	ifstream fin{ "attendance_weekday_500.txt" }; //500개 데이터 입력
+	for (int rollBookIndex = 0; rollBookIndex < 500; rollBookIndex++) {
+		string name, day;
+		fin >> name >> day;
+		updateAttendanceAndScore(name, day);
+	}
+}
+
+void compute() {
+	for (int uniformNumber = 1; uniformNumber <= totalUniformNumber; uniformNumber++) {
+		if (bonusPoint[uniformNumber][wednesday] > 9) {
+			totalPoint[uniformNumber] += 10;
 		}
 
-		if (dat[i][5] + dat[i][6] > 9) {
-			points[i] += 10;
+		if (bonusPoint[uniformNumber][weekend] > 9) {
+			totalPoint[uniformNumber] += 10;
 		}
 
-		if (points[i] >= 50) {
-			grade[i] = 1;
+		if (totalPoint[uniformNumber] >= 50) {
+			memberGrade[uniformNumber] = 2;
 		}
-		else if (points[i] >= 30) {
-			grade[i] = 2;
-		}
-		else {
-			grade[i] = 0;
+		else if (totalPoint[uniformNumber] >= 30) {
+			memberGrade[uniformNumber] = 1;
 		}
 
-		cout << "NAME : " << names[i] << ", ";
-		cout << "POINT : " << points[i] << ", ";
-		cout << "GRADE : ";
-
-		if (grade[i] == 1) {
-			cout << "GOLD" << "\n";
-		}
-		else if (grade[i] == 2) {
-			cout << "SILVER" << "\n";
-		}
-		else {
-			cout << "NORMAL" << "\n";
-		}
+		cout << "NAME : " << memberNameArray[uniformNumber] << ", ";
+		cout << "POINT : " << totalPoint[uniformNumber] << ", ";
+		cout << "GRADE : " << gradeList[memberGrade[uniformNumber]] << "\n";
 	}
 
 	std::cout << "\n";
 	std::cout << "Removed player\n";
 	std::cout << "==============\n";
-	for (int i = 1; i <= id_cnt; i++) {
-
-		if (grade[i] != 1 && grade[i] != 2 && wed[i] == 0 && weeken[i] == 0) {
-			std::cout << names[i] << "\n";
+	for (int uniformNumber = 1; uniformNumber <= totalUniformNumber; uniformNumber++) {
+		if (memberGrade[uniformNumber] == 0 && trainingDayAttendanceCount[uniformNumber] == 0 && weekendAttendanceCount[uniformNumber] == 0) {
+			std::cout << memberNameArray[uniformNumber] << "\n";
 		}
 	}
 }
 
+void loadAndCompute() {
+	load();
+	compute();
+}
+
 int main() {
-	input();
+	loadAndCompute();
 }
